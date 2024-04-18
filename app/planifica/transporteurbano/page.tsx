189b5f9aa9c0cdc9d59c13db27e1ac0f93e9a2f6
Federico Alvarez <fagomezomil@gmail.com>
2024-04-17 21:26:00 -0300

    avance de transporte

--- app/planifica/transporteurbano/page.tsx
+++ app/planifica/transporteurbano/page.tsx
@@ -53,28 +53,43 @@ function Page() {
             <div className="text-xl font-semibold text-white bg-lime-700 px-4 py-2 w-64 rounded-lg absolute -top-2">
               Circuito Las Yungas
             </div>
-            <div className="grid grid-cols-3 mt-8">
-              <div className="inline-grid min-w-max grid-cols-1 gap-1">
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+            <div className="grid grid-cols-5 mt-8 ">
+              <div className="inline-grid  grid-cols-1 gap-1 h-fit">
+                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   Yerba Buena
                 </button>
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   San Javier
                 </button>
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   Raco y El Siambón
                 </button>
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+                <button className=" focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   El Cadillal
                 </button>
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   Tafí Viejo
                 </button>
-                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 w-fit">
+                <button className="focus:text-lime-700 focus:border-lime-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
                   Villa Nougués
                 </button>
               </div>
-              <div className="col-span-2">
+              <div className="col-span-4">
+                <div className="mt-2">
+                  <p className="uppercase text-md font-semibold">Encontrá la linea que se adapte a tu destino</p>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 100
+                  </button>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 102
+                  </button>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 118
+                  </button>
+                  <div className="mt-4">
+                    <iframe className="w-full" width="425" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=-65.27638435363771%2C-26.862208663146948%2C-65.13304710388185%2C-26.7932754116777&amp;layer=mapnik" ></iframe>
+                  </div>
+                </div>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sapiente omnis harum non facere similique delectus, odio ea illum quis debitis totam id iusto dolorum, cum, corporis velit dolor ullam. Dicta in dignissimos exercitationem ut cupiditate cum doloribus, nihil alias illum ab molestias aliquid mollitia, explicabo voluptatibus possimus nisi. Soluta earum sint nam, numquam exercitationem perferendis cum, eius, expedita eum iure dolorem! Dicta iusto impedit porro consequatur architecto. Ipsum nihil tempora, laboriosam dolores consequuntur iusto sit rem quos voluptate a sapiente quo est minima delectus molestiae ad culpa saepe mollitia, debitis odit eaque omnis similique. Non sequi aliquam iste ratione?
                 </p>
@@ -82,7 +97,7 @@ function Page() {
             </div>
           </div>
         </div>
-        
+
       </div>
     </>
   );
