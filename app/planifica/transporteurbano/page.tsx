650eb67152a22a29e2019dd9b67bff5c5fd9366f
Federico Alvarez <fagomezomil@gmail.com>
2024-04-18 11:50:05 -0300

    ajustes para presentacion, sumadas imagenes, creacion de seccion

--- app/planifica/transporteurbano/page.tsx
+++ app/planifica/transporteurbano/page.tsx
@@ -8,12 +8,28 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import Image from "next/image";
-import { FaMapLocationDot, FaRegFilePdf } from "react-icons/fa6";
+import { FaMapLocationDot, FaRegFilePdf, FaRegClock } from "react-icons/fa6";
+import { AiOutlineDollar } from "react-icons/ai";
+import { MdOutlinePinDrop } from "react-icons/md";
 
 function Page() {
   return (
     <>
-      <div className="max-w-7xl w-full mx-auto pt-28 mb-6 pl-6">
+      <div
+        className="bg-cover bg-center bg-no-repeat  grid content-end"
+        style={{
+          backgroundImage: `url('/images/headers/transporte.jpg')`,
+          height: "750px",
+        }}
+      >
+        <div
+          id="titulo-articulo"
+          className="max-w-7xl w-full pl-6 mb-6 mx-auto text-4xl font-bold text-white text-left"
+        >
+          Transporte Urbano
+        </div>
+      </div>
+      <div className="max-w-7xl w-full mx-auto pt-6 mb-6 pl-6">
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
@@ -38,9 +54,6 @@ function Page() {
       </div>
       <div className="mt-6 mb-9  max-w-7xl w-full mx-auto">
         <div className=" ">
-          <p className="mx-6 text-2xl text-14 text-gray-800 mb-6">
-            Transporte Urbano
-          </p>
           <p className="mx-6 mb-8">
             Si querés conocer todos los atractivos que Tucumán tiene para
             ofrecerte y no contás con vehículo propio, o no querés conducir para
@@ -50,7 +63,7 @@ function Page() {
             itinerario.
           </p>
           <div className="rounded-lg bg-white p-6 shadow-lg relative mx-6">
-            <div className="text-xl font-semibold text-white bg-lime-700 px-4 py-2 w-64 rounded-lg absolute -top-2">
+            <div className="text-xl font-semibold text-white bg-lime-700 px-4 py-2 w-fit rounded-lg absolute -top-2">
               Circuito Las Yungas
             </div>
             <div className="grid grid-cols-5 mt-8 ">
@@ -76,7 +89,9 @@ function Page() {
               </div>
               <div className="col-span-4">
                 <div className="mt-2">
-                  <p className="uppercase text-md font-semibold">Encontrá la linea que se adapte a tu destino</p>
+                  <p className="uppercase text-md font-semibold">
+                    Encontrá la línea que se adapte a tu destino
+                  </p>
                   <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
                     Línea 100
                   </button>
@@ -87,17 +102,155 @@ function Page() {
                     Línea 118
                   </button>
                   <div className="mt-4">
-                    <iframe className="w-full" width="425" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=-65.27638435363771%2C-26.862208663146948%2C-65.13304710388185%2C-26.7932754116777&amp;layer=mapnik" ></iframe>
+                    <iframe
+                      className="w-full"
+                      width="425"
+                      height="350"
+                      src="https://www.openstreetmap.org/export/embed.html?bbox=-65.27638435363771%2C-26.862208663146948%2C-65.13304710388185%2C-26.7932754116777&amp;layer=mapnik"
+                    ></iframe>
                   </div>
                 </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sapiente omnis harum non facere similique delectus, odio ea illum quis debitis totam id iusto dolorum, cum, corporis velit dolor ullam. Dicta in dignissimos exercitationem ut cupiditate cum doloribus, nihil alias illum ab molestias aliquid mollitia, explicabo voluptatibus possimus nisi. Soluta earum sint nam, numquam exercitationem perferendis cum, eius, expedita eum iure dolorem! Dicta iusto impedit porro consequatur architecto. Ipsum nihil tempora, laboriosam dolores consequuntur iusto sit rem quos voluptate a sapiente quo est minima delectus molestiae ad culpa saepe mollitia, debitis odit eaque omnis similique. Non sequi aliquam iste ratione?
-                </p>
+                <div className="grid grid-rows-1">
+                  <div className="grid grid-cols-12 w-fit items-center mt-3 mb-3">
+                    <div className="grid-cols-1 w-fit text-4xl text-lime-700">
+                      <AiOutlineDollar />
+                    </div>
+                    <div className="col-span-11 w-fit">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">Costo:</strong>{" "}
+                        Dependiendo hasta qué parte de Yerba Buena decidas
+                        viajar el costo del boleto varía desde $690.-
+                      </p>
+                    </div>
+                    <div className="grid-cols-1 w-fit text-4xl text-lime-700 mt-3">
+                      <MdOutlinePinDrop />
+                    </div>
+                    <div className="col-span-11 w-fit mt-5">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">
+                          Dónde Tomarlo:
+                        </strong>{" "}
+                        Las líneas 118 o 100 circulan por calle Santiago del
+                        Estero, o bien podes esperarlo en terminal de ómnibus.
+                        La línea 102 también tiene parada en la terminal o si
+                        estas en el centro podes esperarlo en calle San Lorenzo.
+                      </p>
+                    </div>
+                    <div className="grid-cols-1 w-fit text-3xl text-lime-700 mt-3">
+                      <FaRegClock />
+                    </div>
+                    <div className="col-span-11 w-fit mt-5">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">
+                          Horarios:
+                        </strong>{" "}
+                        Cualquiera de estas líneas tiene una frecuencia de 10 minutos. 
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg bg-white p-6 shadow-lg relative mx-6 mt-12">
+            <div className="text-xl font-semibold text-white bg-red-700 px-4 py-2 w-fit rounded-lg absolute -top-2">
+              Circuito Valle Calchaquí
+            </div>
+            <div className="grid grid-cols-5 mt-8 ">
+              <div className="inline-grid  grid-cols-1 gap-1 h-fit">
+                <button className="focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  Tafí del Valle
+                </button>
+                <button className="focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  El Mollar
+                </button>
+                <button className="focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  Ampimpa
+                </button>
+                <button className=" focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  Amaicha del Valle
+                </button>
+                <button className="focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  Quilmes
+                </button>
+                <button className="focus:text-red-700 focus:border-red-700 focus:bg-white focus:font-semibold focus:border-2 rounded-md text-md font-medium px-3 py-2 mt-3 mx-1 text-gray-600 block border-1 hover:text-white hover:bg-gray-400 h-fit w-fit">
+                  Colalao del Valle
+                </button>
+              </div>
+              <div className="col-span-4">
+                <div className="mt-2">
+                  <p className="uppercase text-md font-semibold">
+                    Encontrá la línea que se adapte a tu destino
+                  </p>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 100
+                  </button>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 102
+                  </button>
+                  <button className="bg-gray-400 hover:bg-gray-600 rounded-md text-md font-semibold px-3 py-2 mt-3 mx-1 text-white">
+                    Línea 118
+                  </button>
+                  <div className="mt-4">
+                    <iframe
+                      className="w-full"
+                      width="425"
+                      height="350"
+                      src="https://www.openstreetmap.org/export/embed.html?bbox=-65.27638435363771%2C-26.862208663146948%2C-65.13304710388185%2C-26.7932754116777&amp;layer=mapnik"
+                    ></iframe>
+                  </div>
+                </div>
+                <div className="grid grid-rows-1">
+                  <div className="grid grid-cols-12 w-fit items-center mt-3 mb-3">
+                    <div className="grid-cols-1 w-fit text-4xl text-red-700">
+                      <AiOutlineDollar />
+                    </div>
+                    <div className="col-span-11 w-fit">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">Costo:</strong>{" "}
+                        Dependiendo hasta qué parte de Yerba Buena decidas
+                        viajar el costo del boleto varía desde $690.-
+                      </p>
+                    </div>
+                    <div className="grid-cols-1 w-fit text-4xl text-red-700 mt-3">
+                      <MdOutlinePinDrop />
+                    </div>
+                    <div className="col-span-11 w-fit mt-5">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">
+                          Dónde Tomarlo:
+                        </strong>{" "}
+                        Las líneas 118 o 100 circulan por calle Santiago del
+                        Estero, o bien podes esperarlo en terminal de ómnibus.
+                        La línea 102 también tiene parada en la terminal o si
+                        estas en el centro podes esperarlo en calle San Lorenzo.
+                      </p>
+                    </div>
+                    <div className="grid-cols-1 w-fit text-3xl text-red-700 mt-3">
+                      <FaRegClock />
+                    </div>
+                    <div className="col-span-11 w-fit mt-5">
+                      <p>
+                        {" "}
+                        <strong className="uppercase">
+                          Horarios:
+                        </strong>{" "}
+                        Cualquiera de estas líneas tiene una frecuencia de 10 minutos. 
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                
               </div>
             </div>
           </div>
         </div>
-
       </div>
     </>
   );
--- app/tucuman/destinosycircuitos/page.tsx
+++ app/tucuman/destinosycircuitos/page.tsx
@@ -16,7 +16,7 @@ export default function page() {
       <div
         className="bg-cover bg-center bg-no-repeat  grid content-end"
         style={{
-          backgroundImage: `url('/images/headers/nunorco.jpg')`,
+          backgroundImage: `url('/images/headers/destinos.jpg')`,
           height: "750px",
         }}
       >
@@ -50,95 +50,89 @@ export default function page() {
           </BreadcrumbList>
         </Breadcrumb>
       </div>
-      <div className="max-w-7xl w-full mx-auto grid grid-cols-3 gap-12 pl-6">
-        <div className="text-14 text-gray-800 mt-5 col-span-2">
-          <p className="text-2xl">Circuito Las Yungas</p>
-          <br />
-          <p>
-            Se caracteriza por destinos paradisíacos de visita obligada, entre
-            los que destacan San Javier, con sus espectaculares vistas
-            panorámicas desde el magnífico Cristo Bendicente; Villa Nougués que
-            permite pasar una jornada tranquila en una verdadera campiña
-            europea; Yerba Buena que combina la calma de la naturaleza y el
-            ritmo vertiginoso de sus noches; y El Cadillal, ideal para relajarse
-            frente al imponente dique Celestino Gelsi o emprender una aventura
-            en sus aguas. Tafí Viejo permite conocer en profundidad la historia
-            ferroviaria de la provincia, en tanto que San Pablo encierra los
-            vestigios de lo que fue uno de los ingenios azucareros más
-            importantes. Raco, por su parte, te espera con sus tupidos bosques
-            perfectos para una apacible cabalgata, mientras que El Siambón es un
-            destino ideal para conectar con tu fe. Anfama, por último, es un
-            paraje mágico ubicado a 2.000 metros de altura que podrás explorar
-            en grupo, acompañado siempre por un guía.
-          </p>
-          <p className="uppercase font-bold text-12 text-gray-600 xl:mt-8 mb-3">
-            Video
-          </p>
-          <video
-            width="auto"
-            height="400"
-            autoPlay
-            loop
-            muted
-            className=" w-fit"
-          >
-            <source src="/video/video.mp4" type="video/mp4" />
-            Tu navegador no soporta la reproducción de videos.
-          </video>
-        </div>
-        <div className="mt-5">
-          <p className="uppercase font-bold text-12 text-gray-600 xl:mt-8">
-            Galería de imágenes
-          </p>
-          <Image
-            src="/images/galeria/1.jpg"
-            alt=""
-            width={400}
-            height={300}
-            className="mt-3"
-          />
-          <div className="grid grid-cols-2 gap-3 mt-2">
+      <div className="max-w-7xl w-full mx-auto pl-6">
+        <div className="mt-16 grid grid-cols-2">
+          <div className="w-fit justify-self-end">
+            <Image
+              src="/images/galeria/1.jpg"
+              alt=""
+              width={500}
+              height={150}
+            />
+          </div>
+          <div className="mx-6 mt-14 mr-32">
+            <p className="font-bold text-3xl text-gray-600">Circuito Yungas</p>
+            <p className="text-gray-600 mt-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              maiores fuga praesentium doloremque consectetur accusamus quam,
+              nostrum quos magni sunt, dolor consequatur odit rem. Obcaecati
+              nobis modi ducimus harum.
+            </p>
+            <button className="bg-lime-500 rounded-md text-xs font-semibold px-5 py-2 mt-3 text-white">
+              Conocé más aquí
+            </button>
+          </div>
+          <div className="mx-6 mt-32 ml-32">
+            <p className="font-bold text-3xl text-gray-600">Circuito Valle Calchaquí</p>
+            <p className="text-gray-600 mt-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              maiores fuga praesentium doloremque consectetur accusamus quam,
+              nostrum quos magni sunt, dolor consequatur odit rem. Obcaecati
+              nobis modi ducimus harum.
+            </p>
+            <button className="bg-lime-500 rounded-md text-xs font-semibold px-5 py-2 mt-3 text-white">
+              Conocé más aquí
+            </button>
+          </div>
+          <div className="w-fit justify-self-start mt-20">
             <Image
               src="/images/galeria/2.jpg"
               alt=""
-              width={200}
+              width={500}
               height={150}
-              className="mt-1"
             />
+          </div>
+        </div>
+        <div className="mt-16 mb-16 grid grid-cols-2">
+          <div className="w-fit justify-self-end">
+            <Image
+              src="/images/galeria/4.jpg"
+              alt=""
+              width={500}
+              height={150}
+            />
+          </div>
+          <div className="mx-6 mt-14 mr-32">
+            <p className="font-bold text-3xl text-gray-600">Circuito Sur</p>
+            <p className="text-gray-600 mt-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              maiores fuga praesentium doloremque consectetur accusamus quam,
+              nostrum quos magni sunt, dolor consequatur odit rem. Obcaecati
+              nobis modi ducimus harum.
+            </p>
+            <button className="bg-lime-500 rounded-md text-xs font-semibold px-5 py-2 mt-3 text-white">
+              Conocé más aquí
+            </button>
+          </div>
+          <div className="mx-6 mt-32 ml-32">
+            <p className="font-bold text-3xl text-gray-600">Circuito Valle de Choromoro</p>
+            <p className="text-gray-600 mt-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+              maiores fuga praesentium doloremque consectetur accusamus quam,
+              nostrum quos magni sunt, dolor consequatur odit rem. Obcaecati
+              nobis modi ducimus harum.
+            </p>
+            <button className="bg-lime-500 rounded-md text-xs font-semibold px-5 py-2 mt-3 text-white">
+              Conocé más aquí
+            </button>
+          </div>
+          <div className="w-fit justify-self-start mt-20">
             <Image
               src="/images/galeria/3.jpg"
               alt=""
-              width={200}
+              width={500}
               height={150}
-              className="mt-1"
             />
-          </div>
-          <p className="uppercase font-bold text-12 text-gray-600 xl:mt-8">
-            Para descargar
-          </p>
-          <div className="mt-4 ">
-            <div className="flex">
-              <FaMapLocationDot className="text-gray-500 text-5xl" />
-              <div className="ml-3 mt-1">
-                <p className="text-12 font-medium text-gray-600">
-                  Hacé click para descargar
-                </p>
-                <p className="font-bold text-gray-500">
-                  Mapa Circuito Las Yungas
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-4">
-              <FaRegFilePdf className="text-gray-500 text-5xl" />
-              <div className="ml-3 mt-1">
-                <p className="text-12 font-medium text-gray-600">
-                  Hacé click para descargar
-                </p>
-                <p className="font-bold text-gray-500">
-                  Folleto Circuito Las Yungas
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </div>
