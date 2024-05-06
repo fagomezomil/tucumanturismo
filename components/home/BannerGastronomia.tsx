7a20ebefb0c8e684995e0cefacf8d85c164d04ff
Federico Alvarez <fagomezomil@gmail.com>
2024-05-05 23:17:37 -0300

    ajustes banner gastronomia

--- components/home/BannerGastronomia.tsx
+++ components/home/BannerGastronomia.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 import Image from 'next/image';
 
 const imagenes = [
-  '/images/artesanos/ceramica.jpg',
-  '/images/artesanos/textil.jpg',
-  '/images/artesanos/cuero.jpg',
-  '/images/artesanos/simoca.jpg',
+  {url: '/images/gastronomia/empanadas.jpg', texto: 'Conocé la Empanada más rica del país' , nombre: 'Empanadas', },
+  {url: '/images/gastronomia/milanesa.jpg', texto: 'Conocé la Milanesa más rica del mundo' , nombre: 'Milanesa', },
+  {url: '/images/gastronomia/locro.jpg' , texto: 'Conocé el Locro más rico del mundo' , nombre: 'Locro', },
 ];
 
 export default function BannerGastronomia() {
@@ -20,13 +19,13 @@ export default function BannerGastronomia() {
   }, []);
 
   return (
-    <div className="h-[400px] relative">
+    <div className="h-[400px] mt-12">
       <div
-        className="bg-cover bg-center h-[400px] w-full content-center transition-opacity duration-1000 ease-out"
-        style={{ backgroundImage: `url(${imagenes[currentImage]})`, opacity: 1 }}
+        className="bg-cover bg-fixed bg-left bg-no-repeat  h-[400px] w-full content-center transition-opacity duration-1000 ease-out"
+        style={{ backgroundImage: `url(${imagenes[currentImage].url})`, opacity: 1 }}
       >
-        <div className="grid grid-cols-2">
-          <p className="text-4xl font-bold text-white text-center">Título</p>
+        <div className="max-w-7xl w-full pl-6 mb-6 mx-auto grid grid-cols-2">
+          <p className="text-6xl italic font-normal text-white text-right max-w-[500px]">{imagenes[currentImage].texto}</p>
         </div>
       </div>
     </div>
